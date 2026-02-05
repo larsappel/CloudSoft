b787d76510aff40bdd3d8cc58786725617216e4a
Lars Appel <lasse.appel@gmail.com>
2026-02-05 21:43:22 +0100

    Data Annotations and Client-Side Validation

--- Models/Subscriber.cs
+++ Models/Subscriber.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CloudSoft.Models;
 
 public class Subscriber
 {
+    [Required]
+    [StringLength(20, ErrorMessage = "Name cannot exceed 20 characters")]
     public string? Name { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string? Email { get; set; }
 }
